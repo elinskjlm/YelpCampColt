fef5f04cdc997640d5b76fa6e4c0ccf061d40593
elinskjlm <elinskjlm@gmail.com>
2024-03-19 13:44:17 +0200

    still basic setup

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
-    res.send('Hello.')
+    res.render('home')
 })
 
 app.listen(port, () => {
