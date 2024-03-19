d4ae8b271749a25666dbd01b4b60d8a320578121
elinskjlm <elinskjlm@gmail.com>
2024-03-19 16:59:41 +0200

    first model

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
 const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+
 const app = express();
 const port = 3000;
-const path = require('path');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp');
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Connection Error:'));
+db.once('open', () => {
+    console.log('Database Connected.');
+})
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
     res.render('home')
+})
+
+app.get('/makecampground', async (req, res) => {
+    const camp = new Campground({ title: 'Camp1', description: 'just just'});
+    await camp.save()
+    .then(() => {
+       res.send(camp) 
+    })
+    .catch(e => {
+        res.send('WTF01') 
+    })
 })
 
 app.listen(port, () => {
