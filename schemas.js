9a1620be3e0d8df3a216b081c899f9069b3e9ff4
elinskjlm <elinskjlm@gmail.com>
2024-04-15 10:12:53 +0300

    Joi text validation set to 2 where it was 8

--- schemas.js
+++ schemas.js
@@ -3,10 +3,10 @@ module.exports.campgroundSchema = Joi.object({
     campground: Joi.object({
         title:          Joi.string().required().min(2).max(255),
         price:          Joi.number().required().min(0).max(1000000000),
-        image:          Joi.string().required().min(8).max(2048),
-        description:    Joi.string().required().min(8).max(2048),
-        city:           Joi.string().required().min(8).max(2048),
-        state:          Joi.string().required().min(8).max(2048),
+        image:          Joi.string().required().min(2).max(2048),
+        description:    Joi.string().required().min(2).max(2048),
+        city:           Joi.string().required().min(2).max(2048),
+        state:          Joi.string().required().min(2).max(2048),
     }).required()
 });
 
