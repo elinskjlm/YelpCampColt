ed46cce89b8130cf9c1c561053e9056db1c139c6
elinskjlm <elinskjlm@gmail.com>
2024-04-19 21:45:33 +0300

    Logout

--- routes/users.js
+++ routes/users.js
@@ -24,10 +24,20 @@ router.post('/register', catchAsync (async (req, res) => {
 router.get('/login', (req, res) => {
     res.render('users/login');
 })
+
 router.post('/login', passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), (req, res) => {
     req.flash('success', 'Welcome back 🙄');
     res.redirect('/campgrounds');
 })
 
+router.get('/logout', (req, res, next) => {
+    req.logOut(function (err) {
+        if (err) {
+            return next(err);
+        }
+        req.flash('success', 'Logged out 🥾');
+        res.redirect('/campgrounds');
+    });
+})
 
 module.exports = router;
