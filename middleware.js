246df8720d99c49c66c9b776fcaed5e312da61f6
elinskjlm <elinskjlm@gmail.com>
2024-04-20 08:59:50 +0300

    Navbar links for login/logout

--- middleware.js
+++ middleware.js
@@ -1,4 +1,5 @@
 module.exports.isLoggedIn = (req, res, next) => {
+    console.log(`req.user: ${req.user}`);
     if (!req.isAuthenticated()) {
         req.flash('error', 'You must be signed in 🖐🏻');
         return res.redirect('/login');
