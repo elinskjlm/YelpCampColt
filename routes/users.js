2747da863c31dd33d7e3fab78427bdd0714e913c
elinskjlm <elinskjlm@gmail.com>
2024-04-20 10:42:25 +0300

    Signing up also logs in

--- routes/users.js
+++ routes/users.js
@@ -9,12 +9,14 @@ router.get('/register', (req, res) => {
 })
 router.post('/register', catchAsync (async (req, res) => {
     try {
-    const { email, username, password } = req.body;
-    const user = new User({ email, username });
-    const registeredUser = await User.register(user, password);
-    console.log(registeredUser);
-    req.flash('success', 'Welcome to Yelp Camp 🤷🏻‍♂️');
-    res.redirect('/campgrounds');
+        const { email, username, password } = req.body;
+        const user = new User({ email, username });
+        const registeredUser = await User.register(user, password);
+        req.login(registeredUser, err => {
+            if (err) return next(err);
+            req.flash('success', 'Welcome to Yelp Camp 🤷🏻‍♂️');
+            res.redirect('/campgrounds');
+        })
     } catch(e) {
         req.flash('error', e.message);
         res.redirect('register');
