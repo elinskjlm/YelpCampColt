fd502604759f509c1b0402cbbb032d1706068cc2
elinskjlm <elinskjlm@gmail.com>
2024-06-08 14:17:13 +0300

    Added About page

--- app.js
+++ app.js
@@ -117,6 +117,10 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
 app.all('*' , (req, res, next) => {
     next(new ExpressError(`Page not found: ${req.url}`, 404));
 })
